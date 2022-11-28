207e10b636212dfda4ee8e9779684f1f2cc2c6b7
danielkraus14 <danielk14@servimatsrl.com.ar>
2022-11-28 16:30:26 -0300

    removed temporarily auth middlewares for testing

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,7 @@ const {
     categoryController
 } = require('../controllers');
 
-// Import Middlewares
+// Import Auth Middlewares
 const {
     isAuth,
     isAdmin,
@@ -25,30 +25,30 @@ const { userSchema } = require('../controllers/schemas');
 // Users
 routes.post('/signup', userSchema, userController.signUpUser);
 routes.post('/signin', userSchema, userController.signInUser);
-routes.put('/update/:userId', isAuth, isDirective , userController.updateUser);
-routes.delete('/delete/:userId', isAuth, isAdmin , userController.deleteUser);
+routes.put('/update/:userId', userController.updateUser);
+routes.delete('/delete/:userId',  userController.deleteUser);
 
 // Secretaryships
-routes.get('/secretaryships', isAuth, secretaryshipController.getSecretaryships);
-routes.get('/secretaryship/:secretaryshipId', isAuth, secretaryshipController.getSecretaryshipById);
-routes.post('/secretaryships/create', isAuth, isAdmin, secretaryshipController.createSecretaryship);
-routes.put('/secretaryships/update/:secretaryshipId', isAuth, isAdmin, secretaryshipController.updateSecretaryship);
-routes.delete('/secretaryships/delete/:secretaryshipId', isAuth, isAdmin, secretaryshipController.deleteSecretaryship);
+routes.get('/secretaryships', secretaryshipController.getSecretaryships);
+routes.get('/secretaryship/:secretaryshipId', secretaryshipController.getSecretaryshipById);
+routes.post('/secretaryships/create', secretaryshipController.createSecretaryship);
+routes.put('/secretaryships/update/:secretaryshipId', secretaryshipController.updateSecretaryship);
+routes.delete('/secretaryships/delete/:secretaryshipId', secretaryshipController.deleteSecretaryship);
 
 // Categories
 
-routes.get('/categories', isAuth, categoryController.getCategories);
-routes.get('/categories/:categoryId', isAuth, categoryController.getCategoryById);
-routes.post('/categories/create', isAuth, isAdmin, categoryController.createCategory);
-routes.put('/categories/update/:categoryId', isAuth, isAdmin, categoryController.updateCategory);
-routes.delete('/categories/delete/:categoryId', isAuth, isAdmin, categoryController.deleteCategory);
+routes.get('/categories', categoryController.getCategories);
+routes.get('/categories/:categoryId', categoryController.getCategoryById);
+routes.post('/categories/create', categoryController.createCategory);
+routes.put('/categories/update/:categoryId', categoryController.updateCategory);
+routes.delete('/categories/delete/:categoryId', categoryController.deleteCategory);
 
 // Posts
 routes.get('/posts', postController.getPosts);
 routes.get('/posts/:secretaryshipId', postController.getPostsBySecretaryship);
 routes.get('/posts/:categoryId', postController.getPostsByCategory);
-routes.post('/posts/new', isAuth , isComertial, postController.createPost);
-routes.put('/posts/update/:postId', isAuth, isComertial, postController.updatePost);
-routes.delete('/posts/delete/:postId', isAuth, isComertial, postController.deletePost);
+routes.post('/posts/new',  postController.createPost);
+routes.put('/posts/update/:postId', postController.updatePost);
+routes.delete('/posts/delete/:postId', postController.deletePost);
 
 module.exports = routes;
