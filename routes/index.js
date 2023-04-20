b4b9340ffbfa8f039554cf1b6a8dfdcd39db9211
David Kraus <dkraus@infobytesec.com>
2023-04-20 11:41:26 -0300

    Add auth middlewares

--- routes/index.js
+++ routes/index.js
@@ -31,33 +31,33 @@ const { signUpSchema, signInSchema } = require('../controllers/schemas');
 
 // Routes
 
-// Roles - Admin
-routes.get('/roles', isAuth, roleController.getRoles);
-routes.get('/roles/id/:roleId', isAuth, roleController.getRoleById);
+// Roles
+routes.get('/roles', isAuth, isAdmin, roleController.getRoles);
+routes.get('/roles/id/:roleId', isAuth, isAdmin, roleController.getRoleById);
 
-// Users - Admin
-routes.get('/users', isAuth, userController.getUsers);
-routes.get('/users/id/:userId', isAuth, userController.getUserById);
-routes.post('/signup', signUpSchema, userController.signUpUser);
+// Users
+routes.get('/users', isAuth, isAdmin, userController.getUsers);
+routes.get('/users/id/:userId', isAuth, isAdmin, userController.getUserById);
+routes.post('/signup', isAuth, isAdmin, signUpSchema, userController.signUpUser);
 routes.post('/signin', signInSchema, userController.signInUser);
-routes.put('/users/update/:userId', isAuth, userController.updateUser);
-routes.delete('/users/delete/:userId', isAuth, userController.deleteUser);
+routes.put('/users/update/:userId', isAuth, isAdmin, userController.updateUser);
+routes.delete('/users/delete/:userId', isAuth, isAdmin, userController.deleteUser);
 
-// Sections - Admin
+// Sections
 routes.get('/sections', isAuth, sectionController.getSections);
 routes.get('/sections/:sectionSlug', isAuth, sectionController.getSectionBySlug);
-routes.post('/sections/create', isAuth, sectionController.createSection);
-routes.put('/sections/update/:sectionSlug', isAuth, sectionController.updateSection);
-routes.delete('/sections/delete/:sectionSlug', isAuth, sectionController.deleteSection);
+routes.post('/sections/create', isAuth, isAdmin, sectionController.createSection);
+routes.put('/sections/update/:sectionSlug', isAuth, isAdmin, sectionController.updateSection);
+routes.delete('/sections/delete/:sectionSlug', isAuth, isAdmin, sectionController.deleteSection);
 
-// Categories - Admin
+// Categories
 routes.get('/categories', isAuth, categoryController.getCategories);
 routes.get('/categories/:categorySlug', isAuth, categoryController.getCategoryBySlug);
-routes.post('/categories/create', isAuth, categoryController.createCategory);
-routes.put('/categories/update/:categorySlug', isAuth, categoryController.updateCategory);
-routes.delete('/categories/delete/:categorySlug', isAuth, categoryController.deleteCategory);
+routes.post('/categories/create', isAuth, isAdmin, categoryController.createCategory);
+routes.put('/categories/update/:categorySlug', isAuth, isAdmin, categoryController.updateCategory);
+routes.delete('/categories/delete/:categorySlug', isAuth, isAdmin, categoryController.deleteCategory);
 
-// Posts - Editores - Agregar filtro por tags
+// Posts - Agregar filtro por tags
 routes.get('/posts', isAuth, postController.getPosts);
 routes.get('/posts/id/:postId', isAuth, postController.getPostById);
 routes.get('/posts/search', isAuth, postController.searchPosts);
@@ -67,12 +67,12 @@ routes.post('/posts/create', isAuth, postController.createPost);
 routes.put('/posts/update/:postId', isAuth, postController.updatePost);
 routes.delete('/posts/delete/:postId', isAuth, postController.deletePost);
 
-// Tags - Editores
+// Tags
 routes.get('/tags', isAuth, tagController.getTags);
 routes.get('/tags/:tagName', isAuth, tagController.getTagsByName);
 routes.post('/tags/create', isAuth, tagController.createTag);
 
-// Funeral Notices - Editores
+// Funeral Notices
 routes.get('/funeral-notices', isAuth, funeralNoticeController.getFuneralNotices);
 routes.get('/funeral-notices/id/:funeralNoticeId', isAuth, funeralNoticeController.getFuneralNoticeById);
 routes.get('/funeral-notices/search', isAuth, funeralNoticeController.searchFuneralNotice);
@@ -83,7 +83,7 @@ routes.post('/funeral-notices/create', isAuth, funeralNoticeController.createFun
 routes.put('/funeral-notices/update/:funeralNoticeId', isAuth, funeralNoticeController.updateFuneralNotice);
 routes.delete('/funeral-notices/delete/:funeralNoticeId', isAuth, funeralNoticeController.deleteFuneralNotice);
 
-// Print Editions - Editores - Agregar filtro por tags
+// Print Editions - Agregar filtro por tags
 routes.get('/print-editions', isAuth, printEditionController.getPrintEditions);
 routes.get('/print-editions/id/:printEditionId', isAuth, printEditionController.getPrintEditionById);
 routes.get('/print-editions/date/:date', isAuth, printEditionController.getPrintEditionsByDate);
@@ -91,14 +91,14 @@ routes.post('/print-editions/create', isAuth, printEditionController.createPrint
 routes.put('/print-editions/update/:printEditionId', isAuth, printEditionController.updatePrintEdition);
 routes.delete('/print-editions/delete/:printEditionId', isAuth, printEditionController.deletePrintEdition);
 
-// Venues - Editores
+// Venues
 routes.get('/venues', isAuth, venueController.getVenues);
 routes.get('/venues/:venueSlug', isAuth, venueController.getVenueBySlug);
 routes.post('/venues/create', isAuth, venueController.createVenue);
 routes.put('/venues/update/:venueSlug', isAuth, venueController.updateVenue);
-routes.delete('/venues/delete/:venueSlug', isAuth, venueController.deleteVenue);
+routes.delete('/venues/delete/:venueSlug', isAuth, isAdmin, venueController.deleteVenue);
 
-// Events - Editores
+// Events
 routes.get('/events', isAuth, eventController.getEvents);
 routes.get('/events/id/:eventId', isAuth, eventController.getEventById);
 routes.get('/events/search', isAuth, eventController.searchEvents);
@@ -107,22 +107,22 @@ routes.post('/events/create', isAuth, eventController.createEvent);
 routes.put('/events/update/:eventId', isAuth, eventController.updateEvent);
 routes.delete('/events/delete/:eventId', isAuth, eventController.deleteEvent);
 
-// File Folders - Editores
+// File Folders
 routes.get('/file-folders', isAuth, fileFolderController.getFileFolders);
 routes.get('/file-folders/:fileFolderSlug', isAuth, fileFolderController.getFileFolderBySlug);
 routes.post('/file-folders/create', isAuth, fileFolderController.createFileFolder);
 routes.put('/file-folders/update/:fileFolderSlug', isAuth, fileFolderController.updateFileFolder);
 routes.delete('/file-folders/delete/:fileFolderSlug', isAuth, fileFolderController.deleteFileFolder);
 
-// Files - Editores - Agregar epígrafe
+// Files - Agregar epígrafe
 routes.get('/files', isAuth, fileController.getFiles);
 routes.get('/files/:fileId', isAuth, fileController.readFileById);
 routes.post('/files/create', isAuth, fileController.createFile);
 routes.delete('/files/delete/:fileId', isAuth, fileController.deleteFile);
 
-// Records - Directivos - Agregar qué campo se modificó
-routes.get('/records', isAuth, recordController.getRecords);
-routes.get('/records/search', isAuth, recordController.searchRecords);
-routes.get('/records/object/:recordId', isAuth, recordController.getObjectOfRecord);
+// Records - Agregar qué campo se modificó
+routes.get('/records', isAuth, isDirective, recordController.getRecords);
+routes.get('/records/search', isAuth, isDirective, recordController.searchRecords);
+routes.get('/records/object/:recordId', isAuth, isDirective, recordController.getObjectOfRecord);
 
 module.exports = routes;
